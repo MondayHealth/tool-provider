ef4ec220070b856daa522245e2aeddcbdc0c9c3a
Chris Galardi <christophergalardi@gmail.com>
2018-01-15 16:11:50 -0500

    do an async call for infos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
-import { Button } from "@blueprintjs/core";
 import "./App.css";
 
+const APP_NAME = "Provider";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "" };
+  }
+
+  async componentDidMount() {
+    const res = await fetch("/api/1/hello");
+    const result = await res.json();
+
+    if (!result.success) {
+      console.error("Failed to hello!", result);
+    }
+
+    this.setState(result.result);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <nav>
-            <li>Provider DB</li>
-          </nav>
+          <h2 id="app-name">{APP_NAME}</h2>
+          <h4 id="name-display">{this.state.name}</h4>
         </header>
-        <Button>Hello!</Button>
         <p className="App-intro">Hello, world.</p>
       </div>
     );
