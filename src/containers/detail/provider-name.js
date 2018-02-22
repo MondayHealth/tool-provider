1abb124632eb4004d5ac93219808fcfb6ba2456c
Chris Galardi <christophergalardi@gmail.com>
2018-02-21 21:27:42 -0500

    color fiddling

--- src/containers/detail/provider-name.js
+++ src/containers/detail/provider-name.js
@@ -16,15 +16,19 @@ const ProviderName = ({ elt }) => {
       </a>
     );
   } else {
-    globe = <span className={"pt-icon-globe no-website"} />;
+    globe = <span className={"pt-icon-globe icon-off"} />;
   }
+
+  let genderString = elt.gender ? elt.gender : "?";
+  let genderClass = `gender gender-${elt.gender ? "on" : "off"}`;
+
   return (
     <div className={"provider-name-display"}>
       <h3 id={"first"}>{elt.firstName}</h3>
       <h3 id={"middle"}>{elt.middleName}</h3>
       <h3 id={"last"}>{elt.lastName}</h3>
 
-      <h5 id={"gender"}>{elt.gender ? elt.gender : "?"}</h5>
+      <h5 className={genderClass}>{genderString}</h5>
 
       <BoolIcon value={elt.slidingScale} icon={"dollar"} />
       <BoolIcon value={elt.acceptingNewPatients} icon={"person"} />
