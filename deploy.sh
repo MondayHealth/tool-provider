c07eff5463bc823026f872ed6fc02495634a84c4
Chris Galardi <christophergalardi@gmail.com>
2018-04-02 21:08:45 -0400

    update deploy script to fix serviceworker

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+BUCKET="tools.monday.health"
+
 npm run build
 cd build
-aws s3 sync . s3://tools.monday.health --delete
+
+echo ""
+echo "Uploading build..."
+aws s3 sync . s3://${BUCKET} --delete \
+    --acl public-read \
+    --exclude service-worker.js
+
+echo "Uploading service-worker.js"
+aws s3 cp ./service-worker.js s3://${BUCKET}/service-worker.js \
+    --acl public-read \
+    --metadata-directive REPLACE \
+    --cache-control max-age=0,no-cache,no-store,must-revalidate
+
+echo "Done"
